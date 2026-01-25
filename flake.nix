3f37fe30563cac6d953fb4ff01a94707fd63176c
Johan Yngman <johan.yngman@gmail.com>
2026-01-25 14:50:04 +0100

    cuda

--- flake.nix
+++ flake.nix
@@ -32,13 +32,24 @@
                 ormolu
                 pkg-config
                 llvm
+                cudaPackages.cudatoolkit
+                cudaPackages.cuda_nvcc
               ];
+              shell.shellHook = ''
+                export ACCELERATE_LLVM_CLANG=${pkgs.clang}/bin/clang
+                export NIX_LDFLAGS="$NIX_LDFLAGS -L${pkgs.cudaPackages.cudatoolkit}/lib -L${pkgs.cudaPackages.cudatoolkit}/lib/stubs"
+
+                # Runtime: real libcuda.so comes from the driver. On NixOS it's typically here.
+                export LD_LIBRARY_PATH="${
+                  pkgs.lib.makeLibraryPath [ pkgs.cudaPackages.cudatoolkit ]
+                }:/run/opengl-driver/lib:$LD_LIBRARY_PATH"
+              '';
             };
           })
         ];
         pkgs = import nixpkgs {
           inherit system overlays;
-          inherit (haskellNix) config;
+          config = haskellNix.config // { allowUnfree = true; };
         };
         flake = pkgs.blitz.flake { };
       in flake // {
--- lib/Blitz.hs
+++ lib/Blitz.hs
@@ -19,6 +19,10 @@ import Raylib.Util.Colors
 
 type TickAction = IO ()
 
+{-# NOINLINE tickStore #-}
+tickStore :: FS.Store (IORef TickAction)
+tickStore = FS.Store 0
+
 windowWidth, windowHeight :: Int
 windowWidth = 1920
 windowHeight = 1200
@@ -37,7 +41,6 @@ rgba r g b a =
     .|. (Prelude.fromIntegral b `shiftL` 16)
     .|. (Prelude.fromIntegral a `shiftL` 24)
 
--- Accelerate kernel: given frame number, generate fbH x fbW packed RGBA pixels
 renderAcc :: Acc (Scalar Int) -> Acc (Array DIM2 Word32)
 renderAcc t =
   A.generate (A.constant (Z :. fbH :. fbW)) $ \ix ->
@@ -52,16 +55,11 @@ renderAcc t =
           ABits..|. (b `ABits.shiftL` 16)
           ABits..|. (a `ABits.shiftL` 24)
 
--- Minimal upload path: copy [Word32] into your pixel pointer
 copyPixels :: Ptr Word32 -> [Word32] -> IO ()
 copyPixels p = go 0
   where
     go _ [] = pure ()
     go i (w : ws) = pokeByteOff p (i * 4) w >> go (i + 1) ws
-
-{-# NOINLINE tickStore #-}
-tickStore :: FS.Store (IORef TickAction)
-tickStore = FS.Store 0
 
 main :: IO ()
 main = do
@@ -98,21 +96,16 @@ runWindow tickRef = do
   pixels <- mallocArray (fbW * fbH) :: IO (Ptr Word32)
   frameRef <- newIORef (0 :: Int)
 
-  -- Compile once (important even for “minimal”)
   let render1 = CPU.run1 renderAcc
 
   atomicWriteIORef tickRef $ do
     frame <- readIORef frameRef
     modifyIORef' frameRef (+ 1)
 
-    -- Accelerate render (CPU backend)
     let arr :: Array DIM2 Word32
         arr = render1 (A.fromList Z [frame] :: Scalar Int)
 
-    -- Minimal (slow) copy out
     copyPixels pixels (A.toList arr)
-
-    -- Upload + present
     updateTexture tex (castPtr pixels)
 
     sw <- getScreenWidth
