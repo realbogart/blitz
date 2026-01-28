4dd699ea5172f9a33de6ed6098bd292d6999d992
Johan Yngman <johan.yngman@gmail.com>
2026-01-28 23:46:19 +0100

    Iterating on monad

--- lib/Blitz/Draw.hs
+++ lib/Blitz/Draw.hs
@@ -8,9 +8,7 @@ module Blitz.Draw
   )
 where
 
-import Control.Monad (when)
-import Control.Monad.Reader
-import Control.Monad.State.Strict
+import Control.Monad.IO.Class (MonadIO (liftIO))
 import Data.Int (Int32)
 import Data.Vector.Storable.Mutable qualified as VSM
 import Data.Word (Word32)
@@ -28,8 +26,35 @@ data DrawEnv = DrawEnv
     lineTag :: Int32
   }
 
-newtype DrawM a = DrawM {unDrawM :: ReaderT DrawEnv (StateT Int IO) a}
-  deriving newtype (Functor, Applicative, Monad, MonadIO)
+newtype DrawM a = DrawM {unDrawM :: DrawEnv -> Int -> IO (Int, a)}
+
+instance Functor DrawM where
+  {-# INLINE fmap #-}
+  fmap f (DrawM m) = DrawM $ \env s -> do
+    (s', a) <- m env s
+    pure (s', f a)
+
+instance Applicative DrawM where
+  {-# INLINE pure #-}
+  pure a = DrawM $ \_ s -> pure (s, a)
+  {-# INLINE (<*>) #-}
+  DrawM mf <*> DrawM ma = DrawM $ \env s -> do
+    (s', f) <- mf env s
+    (s'', a) <- ma env s'
+    pure (s'', f a)
+
+instance Monad DrawM where
+  {-# INLINE (>>=) #-}
+  DrawM ma >>= f = DrawM $ \env s -> do
+    (s', a) <- ma env s
+    let DrawM mb = f a
+    mb env s'
+
+instance MonadIO DrawM where
+  {-# INLINE liftIO #-}
+  liftIO io = DrawM $ \_ s -> do
+    a <- io
+    pure (s, a)
 
 -- | Run a frame's worth of drawing. Resets index to 0, runs the action,
 -- pads remaining slots with disabled primitives, returns final primitive count.
@@ -61,7 +86,7 @@ runDrawFrame !tagsV !x1sV !y1sV !x2sV !y2sV !sizesV !colorsV !numPrims !circleTa
             lineTag = lineTagVal
           }
   let DrawM m = action
-  !finalIdx <- execStateT (runReaderT m env) 0
+  (finalIdx, _) <- m env 0
   let !n = min finalIdx numPrims
   -- Pad remaining slots with disabled primitives
   padDisabled env n numPrims
@@ -87,11 +112,9 @@ padDisabled !env !start !end = go start
 -- | Draw a circle at (x, y) with radius r and color col
 {-# INLINE drawCircle #-}
 drawCircle :: Float -> Float -> Float -> Word32 -> DrawM ()
-drawCircle !x !y !r !col = DrawM $ do
-  env <- ask
-  !i <- get
-  when (i < env.maxPrims) $ do
-    liftIO $ do
+drawCircle !x !y !r !col = DrawM $ \env !i ->
+  if i < env.maxPrims
+    then do
       VSM.unsafeWrite env.tags i env.circleTag
       VSM.unsafeWrite env.x1s i x
       VSM.unsafeWrite env.y1s i y
@@ -99,16 +122,16 @@ drawCircle !x !y !r !col = DrawM $ do
       VSM.unsafeWrite env.y2s i 0
       VSM.unsafeWrite env.sizes i r
       VSM.unsafeWrite env.colors i col
-    put $! i + 1
+      let !i' = i + 1
+      pure (i', ())
+    else pure (i, ())
 
 -- | Draw a line from (x1, y1) to (x2, y2) with thickness and color
 {-# INLINE drawLine #-}
 drawLine :: Float -> Float -> Float -> Float -> Float -> Word32 -> DrawM ()
-drawLine !lx1 !ly1 !lx2 !ly2 !thickness !col = DrawM $ do
-  env <- ask
-  !i <- get
-  when (i < env.maxPrims) $ do
-    liftIO $ do
+drawLine !lx1 !ly1 !lx2 !ly2 !thickness !col = DrawM $ \env !i ->
+  if i < env.maxPrims
+    then do
       VSM.unsafeWrite env.tags i env.lineTag
       VSM.unsafeWrite env.x1s i lx1
       VSM.unsafeWrite env.y1s i ly1
@@ -116,4 +139,6 @@ drawLine !lx1 !ly1 !lx2 !ly2 !thickness !col = DrawM $ do
       VSM.unsafeWrite env.y2s i ly2
       VSM.unsafeWrite env.sizes i thickness
       VSM.unsafeWrite env.colors i col
-    put $! i + 1
+      let !i' = i + 1
+      pure (i', ())
+    else pure (i, ())
